40ce5d458bf33972ca32459ac8707eb567f0fdbd
melvin.serrano <melvin.serrano@mh.gob.sv>
2019-09-25 13:42:41 -0600

    se incorpora entidad Role

---
+++ src/main/java/com/Login/demo/EntityDTO/Role.java
@@ -0,0 +1,59 @@
+
+package com.Login.demo.EntityDTO;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
+
+/**
+ *
+ * @author melvin.serrano
+ */
+@Entity
+@Table(name= "ROLE")
+public class Role implements Serializable {
+
+	private static final long serialVersionUID = -2969524610059270447L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO, generator="native")
+	@GenericGenerator(name="native",strategy="native")
+	private Long id;
+	
+	@Column
+	private String name;
+	
+	@Column
+	private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+        
+        
+}
